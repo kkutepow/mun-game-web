ba4293153f7385fef7a838da4950f3d30805725d
KirillK <kkutepovgg@gmail.com>
2019-03-17 20:29:48 +0300

    Added assets and card logic exmaple

--- src/app/shared/models/Card.ts
+++ src/app/shared/models/Card.ts
@@ -1,4 +1,5 @@
 import { Player } from './Player';
+import { Table } from './Table';
 
 export class Card {
     id: string;
@@ -6,5 +7,13 @@ export class Card {
     isEquipment: boolean;
     cardName: string;
     availableActions: string[];
-    owner: Player;
+    actions: { 
+        discard : (table: Table) => void;
+        playWithAll : (table: Table) => void;
+        playWithSelf : (table: Table) => void,
+        sendToPlayer : (table: Table, pid: string) => void;
+        playWithPlayer : (table: Table, pid: string) => void;
+        playWithMonster : (table: Table, cid: string) => void;
+    };
+    owner?: Player;
 }
